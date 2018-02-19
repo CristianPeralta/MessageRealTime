ab6e07f0c1b2912a96682f4b8faef7d53b80d68b
Cristian Peralta <cbperaltasegura@gmail.com>
2018-02-19 15:07:35 -0500

    add mode production to minificated

--- webpack.config.js
+++ webpack.config.js
@@ -51,12 +51,4 @@ if(process.env.NODE_ENV === 'production'){
       }
     })
   );
-
-  module.exports.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env':{
-        NODE_ENV:'production'
-      }
-    })
-  );
 }
